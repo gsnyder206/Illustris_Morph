3967738ea556fe58d9ea37f80cb9cef0da074682
AmandaQuirk <amandaquirk@comcast.net>
2016-08-18 15:23:17 -0400

    Update Illustris_random_forest.py

--- Illustris_random_forest.py
+++ Illustris_random_forest.py
@@ -9,7 +9,7 @@ filt='NC-F115W'
 camera='CAMERA0'
 #for file saving
 z=2
-F=115
+Filter=115
 C=0
 
 #data files
@@ -156,7 +156,7 @@ result, labels, label_probability = pyml.randomForestMC(df,iterations=1000)
 #label_probability = probability of label following random forest (N galaxies x N iterations)
 
 #saves the output as a file
-result.to_pickle('/Users/aquirk/Desktop/result_cut_PC_{}_{}_{}.pkl'.format(C,z,F))
-labels.to_pickle('/Users/aquirk/Desktop/labels_cut_PC_{}_{}_{}.pkl'.format(C,z,F))
-label_probability.to_pickle('/Users/aquirk/Desktop/label_probability_cut_PC_{}_{}_{}.pkl'.format(C,z,F))
-PCs.to_pickle('/Users/aquirk/Desktop/pc_cut_{}_{}_{}.pkl'.format(C,z,F))
+result.to_pickle('/Users/aquirk/Desktop/result_cut_PC_{}_{}_{}.pkl'.format(C,z,Filter))
+labels.to_pickle('/Users/aquirk/Desktop/labels_cut_PC_{}_{}_{}.pkl'.format(C,z,Filter))
+label_probability.to_pickle('/Users/aquirk/Desktop/label_probability_cut_PC_{}_{}_{}.pkl'.format(C,z,Filter))
+PCs.to_pickle('/Users/aquirk/Desktop/pc_cut_{}_{}_{}.pkl'.format(C,z,Filter))
